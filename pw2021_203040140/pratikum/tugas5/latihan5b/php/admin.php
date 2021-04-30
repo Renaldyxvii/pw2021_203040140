1687947060e8320c89a3128dac2e0aba73bbf7c0
Renn <76623285+Renaldyxvii@users.noreply.github.com>
2021-04-30 02:19:33 -0700

    menyelesaikan tugas 5

--- pw2021_203040140/pratikum/tugas5/latihan5b/php/admin.php
+++ pw2021_203040140/pratikum/tugas5/latihan5b/php/admin.php
@@ -20,7 +20,6 @@ $games = query("SELECT * FROM equipment_ml");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../css/style.css">
     <title>Equipment ML</title>
 </head>
 <body>
